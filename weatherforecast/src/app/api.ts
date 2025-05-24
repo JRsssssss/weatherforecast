b4ea7131e919a12073c7297acd787a8b7b2d3c14
JRsssssss <tackelzaza@gmail.com>
2025-05-24 20:59:35 +0700

    Reapply "get 30 days weatherforecast"
    This reverts commit 9c3d6e66a4df23e0bb5045000d92f8dc67ae4109.

--- weatherforecast/src/app/api.ts
+++ weatherforecast/src/app/api.ts
@@ -2,7 +2,7 @@
 
 export async function getWeatherdata() {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=16.821123&lon=100.265854&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`)
+        const response = await fetch(`https://pro.openweathermap.org/data/2.5/forecast/climate?lat=16.821123&lon=100.265854&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`)
         const data = await response.json();
         return {data};
     } catch (error) {
