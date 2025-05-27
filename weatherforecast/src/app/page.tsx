b70f6c09207079aa1902eb127723dd2e9288ced5
JRsssssss <tackelzaza@gmail.com>
2025-05-28 05:57:44 +0700

    setup capacitor done

--- weatherforecast/src/app/page.tsx
+++ weatherforecast/src/app/page.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import React, { useEffect, useState } from "react";
-import { getWeatherdata } from "./api";
 
 export default function Home() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const result = await getWeatherdata();
-      console.log(result?.data);
-      setData(result?.data);
+      try {
+        const response = await fetch(`https://pro.openweathermap.org/data/2.5/forecast/climate?lat=16.821123&lon=100.265854&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`);
+        const result = await response.json();
+        console.log(result);
+        setData(result);
+      } catch (error) {
+        console.error(error);
+      }
     }
 
     fetchData();
@@ -18,7 +22,8 @@ export default function Home() {
 
   return (
     <div className="items-center justify-items-center min-h-screen">
-      Hello World!
+      <h1>Hello World!</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
